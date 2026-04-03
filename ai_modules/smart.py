3ea694ea71480160b372b380d03ef62061192c71
Roman Krzmarzick <romankrzmarzick@gmail.com>
2026-04-03 23:34:23 +0200

    Cleaning up.
    In progress. Cleaning up modules and introducing new ones.

--- ai_modules/smart.py
+++ ai_modules/smart.py
@@ -1,11 +1,2 @@
-import random
-
 class SmartAi:
-    def choose_move(self, board):
-        available_spaces = []
-        for row_index, row in enumerate(board):
-            for col_index, _ in enumerate(row):
-                if board[row_index][col_index] == None:
-                    available_spaces.append((row_index, col_index)) 
-        return random.choice(available_spaces)
-
+    
